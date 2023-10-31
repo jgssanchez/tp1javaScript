e2dfea4bd92e70a0856732fe0a34c4f8fc584c14
Julio Sanchez <jgs.sanchez94@gmail.com>
2023-10-30 22:22:47 -0300

    ejercicio7 listo

---
+++ js/ejercicio7.js
@@ -0,0 +1,11 @@
+let num3 = parseInt(prompt('ingrese el primero de 3 numero para saber cual es mayor:'));
+let num4 = parseInt(prompt('ingrese el segundo numero'))
+let num5 =parseInt(prompt('ingrese el tercer numero'))
+
+if (num3 > num4 && num3 > num5){ 
+    console.log('El numero '+num3+' es el mayor de los 3 ' );
+}else if (num4 > num3 && num4 > num5){
+    console.log('El numero '+num4+' es el mayor de los 3 ' );
+} else{
+    console.log('El numero '+num5+' es el mayor de los 3 ' );
+}
