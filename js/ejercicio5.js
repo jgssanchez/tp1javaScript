dad6b5e6b81574e297f6b6e4b8612ee6df1cebfa
Julio Sanchez <jgs.sanchez94@gmail.com>
2023-10-30 22:04:36 -0300

    ejercicio5 listo

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt("Ingrese el primer número:"))
+let numero2 = parseInt(prompt("Ingrese el segundo número:"));
+console.log("La suma de los números es: " + (numero1 + numero2));
