94c395748bacdff36152543e3a1fb8284fd9438c
Julio Sanchez <jgs.sanchez94@gmail.com>
2023-10-30 22:15:46 -0300

    ejercicio6 listo

---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+let numero3 = parseInt(prompt("Ingrese primer numero:"));
+let numero4 = parseInt(prompt("Ingrese segundo numero:"));
+
+if (numero3 > numero4) {
+    console.log('El numero ' +numero3+ ' es mayor que ' + numero4);
+  } else {
+    console.log('El numero ' +numero4+ ' es mayor que ' + numero3 )
+  }
